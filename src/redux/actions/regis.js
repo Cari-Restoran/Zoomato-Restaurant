301294d05e9e6042f0ad4a395497fbb50c5d923c
Resha Puspita <reshapuspita734@gmail.com>
2020-07-08 19:08:43 +0700

    create function on actions/regis

---
+++ src/redux/actions/regis.js
@@ -0,0 +1,22 @@
+function regis(formData, history) {
+  return async (dispatch) => {
+    const url = "https://5ef168ca1faf160016b4d5b5.mockapi.io/api/users";
+    const options = {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+
+    const response = await fetch(url, options);
+    await response.json();
+
+    if (response.status === 201) {
+      alert("Pendaftaran berhasil");
+      history.push("/login");
+    }
+  };
+}
+
+export { regis };
