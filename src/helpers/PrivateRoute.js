b0b80828fcfdb243ad10383130f0ee524856f82e
Resha Puspita <reshapuspita734@gmail.com>
2020-07-09 06:14:19 +0700

    fix path private route

--- src/helpers/PrivateRoute.js
+++ src/helpers/PrivateRoute.js
@@ -11,7 +11,7 @@ function PrivateRoute({ children, ...rest }) {
         ) : (
           <Redirect
             to={{
-              pathname: "/login",
+              pathname: "/",
               state: { from: location },
             }}
           />
