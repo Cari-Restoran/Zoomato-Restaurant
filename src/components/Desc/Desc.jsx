046832c352883e00bbf96574ee729bc45f0ee192
sinta3 <sinta.tri38@gmail.com>
2020-07-09 09:04:22 +0700

    revisi

--- src/components/Desc/Desc.jsx
+++ src/components/Desc/Desc.jsx
@@ -5,44 +5,73 @@ import {fetchDetail} from '../../redux/actions'
 import { connect } from "react-redux";
 import {useParams} from 'react-router-dom'
 
+import styled from 'styled-components'
+const Div = styled.div`
+    width:500px;
+    display:flex;
+    margin:auto;
+    margin-top:50px;
+
+`;
+const Card = styled.div`
+    width:300px;
+    & img {
+        max-width:200px;
+    }
+`;
+
+const Item = styled.div`
+    display:flex;
+    flex-direction:column;
+`;
+
 function Desc(props) {
     // let index = 0;
     let {id} = useParams();
+
     useEffect(() => {
         props.dispatch(fetchMenu(id));
+        // eslint-disable-next-line
+    }, []);
+
+    useEffect(() => {
         props.dispatch(fetchDetail(id))
         // eslint-disable-next-line
     }, []);
-    console.log(props)
+
+    function input(){
+        return (
+            <Card>
+                    <img src={props.detail.featured_image} alt="food"/>
+                    <h3>{props.detail.average_cost_for_two}</h3>
+                    <h3>{props.detail.location.address}</h3>
+                    <h3>{props.detail.name}</h3>
+            </Card>
+        )
+    };
+  
     return (
-        <div>
-            {props.desc !== undefined && 
-            props.desc.map((item)=> {
-                return (
-                    <div>
-
-                   
-                    <h3>{item.daily_menu.name}</h3>
-                    {/* <h3>{props.detail.establishment.featured_image}</h3> */}
-                    </div>
-                )
-
-            })}
-
-`     
-            
-            {/* {props.detail !== undefined && 
-            // props.detail.map((item)=> {
-                // return (
-
-                <h3>{props.detail.establishment.featured_image}</h3>
-
+        <Div>
+            {props.detail.R !== undefined &&
+            input()
             }
 
-        */}
-        </div>
+            <div>
+
+            {props.desc !== undefined &&
+            props.desc.map((item )=> {
+                return (
+                    <Item >                  
+                    <h4>{item.daily_menu.name}</h4>
+                   </Item>
+                )
+             })}
+            </div>
+       
+        </Div>
     )
-}
+}      
+
 
 const mapStateToProps = (state) => {
     console.log(state)
