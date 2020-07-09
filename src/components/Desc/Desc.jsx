20c5ded52022dbabcab0a51b773c637780a6a749
agusTrih <ant3dong@gmail.com>
2020-07-09 08:52:52 +0700

    add home jumbotron

--- src/components/Desc/Desc.jsx
+++ src/components/Desc/Desc.jsx
@@ -5,27 +5,27 @@ import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 
 function Desc(props) {
-  // let index = 0;
-  let { id } = useParams();
-  useEffect(() => {
-    props.dispatch(fetchMenu(id));
-    props.dispatch(fetchDetail(id));
-    // eslint-disable-next-line
-  }, []);
-  console.log(props);
-  return (
-    <div>
-      {props.desc !== undefined &&
-        props.desc.map((item) => {
-          return (
-            <div>
-              <h3>{item.daily_menu.name}</h3>
-              {/* <h3>{props.detail.establishment.featured_image}</h3> */}
-            </div>
-          );
-        })}
-      `
-      {/* {props.detail !== undefined && 
+    // let index = 0;
+    let { id } = useParams();
+    useEffect(() => {
+        props.dispatch(fetchMenu(id));
+        props.dispatch(fetchDetail(id));
+        // eslint-disable-next-line
+    }, []);
+    console.log(props);
+    return (
+        <div>
+            {props.desc !== undefined &&
+                props.desc.map((item) => {
+                    return (
+                        <div>
+                            <h3>{item.daily_menu.name}</h3>
+                            {/* <h3>{props.detail.establishment.featured_image}</h3> */}
+                        </div>
+                    );
+                })}
+            `
+            {/* {props.detail !== undefined && 
             // props.detail.map((item)=> {
                 // return (
 
@@ -34,16 +34,16 @@ function Desc(props) {
             }
 
         */}
-    </div>
-  );
+        </div>
+    );
 }
 
 const mapStateToProps = (state) => {
-  console.log(state);
-  return {
-    // detail: state.desc,
-    desc: state.desc.daily_menus,
-    detail: state.detail,
-  };
+    console.log(state);
+    return {
+        // detail: state.desc,
+        desc: state.desc.daily_menus,
+        detail: state.detail,
+    };
 };
 export default connect(mapStateToProps, null)(Desc);
